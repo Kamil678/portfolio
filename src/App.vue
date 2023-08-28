bb396c1bd418e735ce2534a9381b6ac2975f6819
Kamil678 <kamilpigulak@interia.pl>
2023-08-28 18:07:09 +0200

    base configuration

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
-<script setup lang="ts"></script>
-
-<template>Home Page</template>
+<template><RouterView /></template>
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
 
 <style scoped></style>
